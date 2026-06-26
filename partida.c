7526b6bd4b7641007c603cbacede4f5ea5ba0b8a
jg-lima <lucenajoao2005@gmail.com>
2026-06-26 10:16:23 -0300

    Foi criada a função configuraJogadores(), que faz com que o jogador escolha se vai jogar de O ou X e deixa a matriz vazia com 0 em todos os indices

---
+++ partida.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "jogo.h"
+
+void configuraJogadores(Partida *p){
+    int escolha = 0;
+
+    printf("Digite '1' para jogar com O ou '4' para jogar com X: ");
+
+    while (escolha != 1 && escolha != 4){
+        scanf("%d", &escolha);
+        if (escolha != 1 || escolha != 4){
+            printf("Escolha inválida, Digite novamente: ");
+        }
+    }
+
+    if (escolha == 4) {
+        p->jogador1.tipo = 4;
+        p->jogador2.tipo = 1;
+
+    } else {
+        p->jogador1.tipo = 1;
+        p->jogador2.tipo = 4;
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            p->tab.M[i][j] = 0;
+        }
+}
