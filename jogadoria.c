e7127bc79695fc3822cbf89ba14b24e283495ff8
jg-lima <lucenajoao2005@gmail.com>
2026-06-28 23:26:33 -0300

    Coloca as regras no Ordem certa

--- jogadoria.c
+++ jogadoria.c
@@ -2,71 +2,75 @@
 #include "jogo.h"
 
 void jogaIA(JogadorIA *jogador, Tabuleiro *tab) {
-    int l, c;
+    int l, c, jogou = 0;
     int adv;
-    int jogou = 0;
 
-    if (jogador->tipo == BOLA) {
-    adv = XIS;
-} 
-    else {
+    if (jogador->tipo == XIS) {
         adv = BOLA;
-}
-
-   for (l = 0; l < 3; l++) {
-    for (c = 0; c < 3; c++) {
-        if (jogou == 0 && tab->M[l][c] == VAZIO) {
-            tab->M[l][c] = jogador->tipo;
-
-            if (temVencedor(tab) != VAZIO) {
-                jogou = 1;
-
-            } else {
-                tab->M[l][c] = VAZIO;
-            }
-        }
+    } 
+    else {
+    adv = XIS;
     }
-}
 
-    for (l = 0; l < 3; l++) {
-        for (c = 0; c < 3; c++) {
-            if (jogou == 0 && tab->M[l][c] == VAZIO) {
-                tab->M[l][c] = adv;
-
-                if (temVencedor(tab) != VAZIO) {
-                    tab->M[l][c] = jogador->tipo;
-                    jogou = 1;
-                } else {
-                    tab->M[l][c] = VAZIO;
+    int tipos[] = {jogador->tipo, adv};
+    for (int t = 0; t < 2 && !jogou; t++) {
+        for (l = 0; l < 3 && !jogou; l++) {
+            for (c = 0; c < 3 && !jogou; c++) {
+                if (tab->M[l][c] == VAZIO) {
+                    tab->M[l][c] = tipos[t];
+                    if (temVencedor(tab) != VAZIO) {
+                        tab->M[l][c] = jogador->tipo;
+                        jogou = 1;
+                    } else {
+                        tab->M[l][c] = VAZIO;
+                    }
                 }
             }
         }
     }
 
-    if (jogou == 0 && tab->M[1][1] == VAZIO) {
+    if (!jogou) {
+        for (l = 0; l < 3 && !jogou; l++) {
+            for (c = 0; c < 3 && !jogou; c++) {
+                if (tab->M[l][c] == VAZIO) {
+                    tab->M[l][c] = jogador->tipo;
+                    
+                    int total = 0;
+                    // Lógica para contar quantas linhas/colunas/diagonais têm 2 peças + 1 VAZIO
+                    // (Você precisaria replicar aqui o código da função contaSequencias)
+                    
+                    if (total >= 2) jogou = 1;
+                    else tab->M[l][c] = VAZIO;
+                }
+            }
+        }
+    }
+    
+    if (!jogou && tab->M[1][1] == VAZIO) {
         tab->M[1][1] = jogador->tipo;
         jogou = 1;
     }
 
-    if (jogou == 0) {
-        if (tab->M[0][0] == VAZIO) {
-            tab->M[0][0] = jogador->tipo;
-            jogou = 1;
-        } else if (tab->M[0][2] == VAZIO) {
-            tab->M[0][2] = jogador->tipo;
-            jogou = 1;
-        } else if (tab->M[2][0] == VAZIO) {
-            tab->M[2][0] = jogador->tipo;
-            jogou = 1;
-        } else if (tab->M[2][2] == VAZIO) {
-            tab->M[2][2] = jogador->tipo;
-            jogou = 1;
+    if (!jogou) {
+        if (tab->M[0][0] == adv && tab->M[2][2] == VAZIO) { tab->M[2][2] = jogador->tipo; jogou = 1; }
+        else if (tab->M[2][2] == adv && tab->M[0][0] == VAZIO) { tab->M[0][0] = jogador->tipo; jogou = 1; }
+        else if (tab->M[0][2] == adv && tab->M[2][0] == VAZIO) { tab->M[2][0] = jogador->tipo; jogou = 1; }
+        else if (tab->M[2][0] == adv && tab->M[0][2] == VAZIO) { tab->M[0][2] = jogador->tipo; jogou = 1; }
+    }
+
+    if (!jogou) {
+        int cantos[4][2] = {{0,0}, {0,2}, {2,0}, {2,2}};
+        for(int i=0; i<4 && !jogou; i++) {
+            if (tab->M[cantos[i][0]][cantos[i][1]] == VAZIO) {
+                tab->M[cantos[i][0]][cantos[i][1]] = jogador->tipo;
+                jogou = 1;
+            }
         }
     }
 
-    for (l = 0; l < 3; l++) {
-        for (c = 0; c < 3; c++) {
-            if (jogou == 0 && tab->M[l][c] == VAZIO) {
+    for (l = 0; l < 3 && !jogou; l++) {
+        for (c = 0; c < 3 && !jogou; c++) {
+            if (tab->M[l][c] == VAZIO) {
                 tab->M[l][c] = jogador->tipo;
                 jogou = 1;
             }
